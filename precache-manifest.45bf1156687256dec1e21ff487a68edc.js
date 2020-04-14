29218239a9b610584a43c4af2b53e206f83d8ddb
Stanislav Kurinec <stenly.kurinec@gmail.com>
2020-04-14 08:20:26 +0200

    Updates

---
+++ precache-manifest.45bf1156687256dec1e21ff487a68edc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bf978e5fd4bc2d29453fa15e71366c",
+    "url": "/nasobilka/index.html"
+  },
+  {
+    "revision": "d90d394835675f63bb86",
+    "url": "/nasobilka/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "09d19b480b8bb6362eb8",
+    "url": "/nasobilka/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d90d394835675f63bb86",
+    "url": "/nasobilka/static/js/2.0e567ad7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nasobilka/static/js/2.0e567ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d19b480b8bb6362eb8",
+    "url": "/nasobilka/static/js/main.30e1def9.chunk.js"
+  },
+  {
+    "revision": "50b30312615d1e968759",
+    "url": "/nasobilka/static/js/runtime-main.1d8c8b0b.js"
+  }
+]);
