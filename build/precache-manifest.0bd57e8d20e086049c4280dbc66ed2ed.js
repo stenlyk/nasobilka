2f8488093eaac4c7a5b9a069ea51382fd7e14731
Stanislav Kurinec <stenly.kurinec@gmail.com>
2020-04-14 07:52:41 +0200

    Build add

---
+++ build/precache-manifest.0bd57e8d20e086049c4280dbc66ed2ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e36543fb046be17f5748ac944cf3e5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d90d394835675f63bb86",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "31a4ad5e970045d83c61",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d90d394835675f63bb86",
+    "url": "/static/js/2.0e567ad7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0e567ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a4ad5e970045d83c61",
+    "url": "/static/js/main.122ba65e.chunk.js"
+  },
+  {
+    "revision": "a294503c3b5282515fe3",
+    "url": "/static/js/runtime-main.4e9cbeda.js"
+  }
+]);
