6bff93d4442caacb07287c502b68fe0ac995c290
Stanislav Kurinec <stenly.kurinec@gmail.com>
2021-11-30 19:19:53 +0100

    Update
    counting to 30

--- src/Start.js
+++ src/Start.js
@@ -1,29 +1,17 @@
 import React from "react";
 
 export const nums = [
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
-  16,
-  17,
-  18,
-  19,
-  20,
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
 
-export const basicMathNums = ["0-10", "0-20", "0-100", "100-999", "1000-9999"];
+export const basicMathNums = [
+  "0-10",
+  "0-20",
+  "0-30",
+  "0-100",
+  "100-999",
+  "1000-9999",
+];
 
 export default class Start extends React.Component {
   constructor(props) {
